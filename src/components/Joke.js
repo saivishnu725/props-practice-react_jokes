ccc67f8fe6243a060bbc5a686c9b3f53705f4e44
saivishnu725 <saivishnu725@gmail.com>
2023-06-30 19:48:12 +0530

    some jokes

---
+++ src/components/Joke.js
@@ -0,0 +1,26 @@
+function Joke({setup, punchline}) {
+
+}
+
+export default Joke;
+
+
+
+/* Challenge:
+
+    - Each Joke should receive a "setup" prop and a "punchline" prop
+      and render those however you'd like
+- Use your favorite 2-part jokes (setup & punchline), or check
+  jokes.md file for some examples.
+
+
+
+EXTRA CREDIT:
+Some jokes are only a punchline with no setup:
+
+E.g.: "It’s hard to explain puns to kleptomaniacs because 
+they always take things literally."
+
+If you don't pass in a "question" prop, how might you make it only 
+show the punchline?
+*/
