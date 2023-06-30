e8a227cc48a0e3fa865fd65fb9f1006fb7015636
saivishnu725 <saivishnu725@gmail.com>
2023-06-30 21:39:16 +0530

    added all 5 jokes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,35 @@ function App() {
   return (
     <div>
       <h1 className="center"> Jokes </h1>
+
       <Joke
         number="1"
         setup="I got my daughter a fridge for her birthday."
         punchline="I can't wait to see her face light up when she opens it."
+      />
+
+      <Joke
+        number="2"
+        setup="How did the hacker escape the police?"
+        punchline="He just ransomware!"
+      />
+
+      <Joke
+        number="3"
+        setup="Why don't pirates travel on mountain roads?"
+        punchline="Scurvy."
+      />
+
+      <Joke
+        number="4"
+        setup="Why do bees stay in the hive in the winter?"
+        punchline="Swarm."
+      />
+
+      <Joke
+        number="5"
+        setup="What's the best thing about Switzerland?"
+        punchline="I don't know, but the flag is a big plus!"
       />
     </div>
   );
