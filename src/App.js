1729cad4e86006a98b543f4b089ba68606058c36
saivishnu725 <saivishnu725@gmail.com>
2023-07-07 18:42:22 +0530

    passing non-string props values

--- src/App.js
+++ src/App.js
@@ -7,37 +7,37 @@ function App() {
       <h1 className="center"> Jokes </h1>
 
       <Joke
-        number="1"
+        number={1}
         setup="I got my daughter a fridge for her birthday."
         punchline="I can't wait to see her face light up when she opens it."
       />
 
       <Joke
-        number="2"
+        number={2}
         setup="How did the hacker escape the police?"
         punchline="He just ransomware!"
       />
 
       <Joke
-        number="3"
+        number={3}
         setup="Why don't pirates travel on mountain roads?"
         punchline="Scurvy."
       />
 
       <Joke
-        number="4"
+        number={4}
         setup="Why do bees stay in the hive in the winter?"
         punchline="Swarm."
       />
 
       <Joke
-        number="5"
+        number={5}
         setup="What's the best thing about Switzerland?"
         punchline="I don't know, but the flag is a big plus!"
       />
 
       {/* a joke with only setup */}
-      <Joke number="6" setup="I was addicted to soap, but I'm clean now." />
+      <Joke number={6} setup="I was addicted to soap, but I'm clean now." />
     </div>
   );
 }
