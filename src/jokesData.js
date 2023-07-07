8d8cb7d9a740481333c8c43b5947dcf4b008c78b
saivishnu725 <saivishnu725@gmail.com>
2023-07-07 19:23:41 +0530

    added number to all the jokes

--- src/jokesData.js
+++ src/jokesData.js
@@ -1,25 +1,31 @@
 const a = [
   {
+    number: 1,
     setup: "I got my daughter a fridge for her birthday.",
     punchline: "I can't wait to see her face light up when she opens it.",
   },
   {
+    number: 2,
     setup: "How did the hacker escape the police?",
     punchline: "He just ransomware!",
   },
   {
+    number: 3,
     setup: "Why don't pirates travel on mountain roads?",
     punchline: "Scurvy.",
   },
   {
+    number: 4,
     setup: "Why do bees stay in the hive in the winter?",
     punchline: "Swarm.",
   },
   {
+    number: 5,
     setup: "What's the best thing about Switzerland?",
     punchline: "I don't know, but the flag is a big plus!",
   },
   {
+    number: 6,
     setup: "I was addicted to soap, but I'm clean now.",
   },
 ];
